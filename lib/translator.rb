9a65f91d1b1a8e984e0c92978c1577ed6c6c1a9f
Jo L <joseph@josephchilin.com>
2020-04-16 20:35:56 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,80 @@
 # require modules here
+require 'pry'
+require 'yaml'
 
-def load_library
+
+# angel:
+#   - "O:)"      
+#   - "☜(⌒▽⌒)☞"
+# => "angel" => ["O:)", - "☜(⌒▽⌒)☞" ]
+
+  # happy:
+  #   - ":)"
+  #   - "(＾ｖ＾)"
+
+  # {
+  #     'happy' => {
+  #       :english => ":)",
+  #       :japanese => "(＾ｖ＾)"
+  #     }
+  # }
+
+def load_library(yaml_library)
   # code goes here
+  library = YAML.load_file(yaml_library)
+  translation = {}
+  
+  library.each do |meaning, language|
+    translation[meaning] = {}
+    eng_emoji = language[0]
+    jap_emoji = language[1]
+    
+    translation[meaning][:english] = eng_emoji
+    translation[meaning][:japanese] = jap_emoji
+  end
+  translation
 end
 
-def get_japanese_emoticon
+  # {
+  #     'happy' => {
+  #       :english => ":)",
+  #       :japanese => "(＾ｖ＾)"
+  #     }
+  # }
+
+def get_japanese_emoticon(yaml_library, english_emoticon)
   # code goes here
+    library = load_library(yaml_library)
+  
+    library.each do |meaning, language|
+      if language[:english] == english_emoticon
+        return language[:japanese]
+        
+      end
+    
+    end
+  
+    "Sorry, that emoticon was not found"
+    
 end
 
-def get_english_meaning
+def get_english_meaning(yaml_library, japanese_emoticon)
   # code goes here
+    library = load_library(yaml_library)
+  
+    library.each do |meaning, language|
+      if language[:japanese] == japanese_emoticon
+        return meaning
+      end
+      
+    end
+
+    "Sorry, that emoticon was not found"
+  
 end
+
+
+
+
+
+
